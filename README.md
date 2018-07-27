a09b77ef42727d5192228e4c7faac930789209bd
Vivek Sharma <viveksh09876@gmail.com>
2018-07-27 16:44:48 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# ReduxWeatherApp
+
+
+### Getting Started
+
+There are two methods for getting started with this repo.
+
+#### Familiar with Git?
+Checkout this repo, install dependencies, then start the gulp process with the following:
+
+```
+> git clone https://github.com/StephenGrider/ReduxWeatherApp.git
+> cd ReduxSimpleStarter
+> npm install
+> npm start
+```
+
---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = 'd2777b4e8577f03742d637ce66e11cbc';
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
